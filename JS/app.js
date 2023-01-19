a58c949ee875b5a5d39ad8bbe0066283c4e1dbda
Geraldo Júnior <geraldo.gj63@gmail.com>
2023-01-19 02:55:50 -0300

    Alterei o background + iniciei o menu mobile
    modifiquei a cor do background e adicionei um background-gradient apenas no home
    
    menu mobile ainda esta incompleto

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,28 @@
+window.addEventListener('scroll', function() {
+    let scroll = this.document.querySelector('.div-arrow-img');
+    scroll.classList.toggle('active', this.window.scrollY > 250);
+});
+
+let menuMobile = document.querySelector('.menu-mobile-section');
+
+// function toggleMenu() {
+//     let nav = document.getElementById('nav');
+//     nav.classList.toggle('active');
+// }
+// menuMobile.addEventListener('click', toggleMenu);
+
+
+
 function menuShow() {
     let menuMobile = document.querySelector('.menu-mobile-section');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.menu-mobile-button-imagem').src = "IMG/menuMobile/menuMobile-closed.png";
+        document.querySelector('#menu-mobile-button-img').src = "IMG/menuMobile/menuMobile-closed.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.menu-mobile-button-imagem').src = "IMG/menuMobile/menuMobile-open.png";
+        document.querySelector('#menu-mobile-button-img').src = "IMG/menuMobile/menuMobile-open.png";
     }
-}
+};
 
 
 
