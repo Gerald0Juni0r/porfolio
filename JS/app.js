b8f7ab3e1163fea7ba93ced494d1ff1ca8cdc35b
Geraldo Júnior <geraldo.gj63@gmail.com>
2023-01-19 18:58:46 -0300

    Adicionei animação em cada section
    O site agora possui animação cada vez que usa-se o scroll

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,94 @@
+// Botão VoltaTopo desaparece ao chegar perto do home
 window.addEventListener('scroll', function() {
     let scroll = this.document.querySelector('.div-arrow-img');
     scroll.classList.toggle('active', this.window.scrollY > 250);
 });
+
+// Animação de surgimento das informações de cada section
+// USANDO DEBOUNCE FUNCTION
+let debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        let context = this;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if(callNow) func.apply(context, args);
+    };
+};
+
+let target = document.querySelectorAll('[data-anime]');
+let animationClass = 'animate';
+
+function animeScroll() {
+    let windowTop =window.pageYOffset + (window.innerHeight * 3) / 4;
+    target.forEach(function(element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    });
+}
+
+animeScroll();
+
+// ATIVANDO O DEBOUNCE FUNCTION
+if (target.length) {
+    window.addEventListener('scroll', debounce(function() {
+        animeScroll();
+    }, 150));
+} 
+
+
+
+
+
+
+
+
+
+
+
+// function anime() {
+//     const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
+//     target.forEach(function(element) {
+//         if((windowTop) > element.offsetTop) {
+//             element.classList.add('animar')
+//         } else {
+//             element.classList.remove('animar')
+//         }
+
+//     })
+// }
+// if(target.length) {
+// window.addEventListener('scroll', debounce(function(){
+//     // console.log('conte')
+//     anime()
+//     console.log('dsad')
+// }, 200));
+// }
+// function debounce(fn, delay) {
+//     let timer = null
+//     return function() {
+//         clearTimeout(timer)
+//         timer = setTimeout(function() {
+//             fn()
+//         }, delay)
+//     }
+// }
+
+
+
+
+
+
+// MENU MOBILE
+
 
 let menuMobile = document.querySelector('.menu-mobile-section');
 
